25a6f91d7aba272abd20722b5ce8fae4ae752def
a01659224B <a01659224@tec.mx>
2023-07-03 01:08:58 -0600

    complemento html

---
+++ logica.js
@@ -0,0 +1,20 @@
+function validarNumeroCelular() {
+    var telefonoInput = document.getElementById("telefono");
+    var telefono = telefonoInput.value;
+    if (telefono.length !== 10) {
+        alert("El número de celular debe tener 10 dígitos.");
+        telefonoInput.focus();
+    }
+}
+
+var checkbox = document.getElementById("tyc");
+
+    function validarFormulario(event) {
+        if (!checkbox.checked) {
+            alert("Debe aceptar los términos y condiciones para enviar el formulario.");
+            event.preventDefault(); 
+        }
+    }
+
+    var formulario = document.querySelector("form");
+    formulario.addEventListener("submit", validarFormulario);
